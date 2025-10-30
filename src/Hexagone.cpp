53ab1a59e8afe36368f4bc102021a2e06593a57f
Boucanier <chironjules.78@gmail.com>
2025-10-30 15:57:51 +0100

    Boucle affichage du type

--- src/Hexagone.cpp
+++ src/Hexagone.cpp
@@ -3,60 +3,49 @@
 void Hexagone::afficher() const
 {
     std::cout << "Hexagone(" << x << "," << y << "," << z << ")"
-                << (est_recouvert ? " [recouvert]" : "") << "\n";
-    // Vérifie si c'est une Carrière, Quartier ou Place
-    if (type_ == TypeHex::Carriere)
-    {
-        texte_couleur(BLEU);
-        std::cout << "  Carrière\n";
-    }
-    else if (this->isQuartier())
+              << (est_recouvert ? " [recouvert]" : "") << "\n";
+
+    // Affichage du type
+    if (this->isQuartier())
     {
         std::cout << "  Quartier : ";
-        if (type_ == TypeHex::Habitation){
-            texte_couleur(CYAN);
-            std::cout << "Habitation\n";
-        }
-        if (type_ == TypeHex::Marche){
-            texte_couleur(JAUNE);
-            std::cout << "Marché\n";
-        }
-        if (type_ == TypeHex::Temple){
-            texte_couleur(MAGENTA);
-            std::cout << "Temple\n";
-        }
-        if (type_ == TypeHex::Caserne){
-            texte_couleur(ROUGE);
-            std::cout << "Caserne\n";
-        }
-        if (type_ ==  TypeHex::Jardin){
-            texte_couleur(VERT);
-            std::cout << "Jardin\n";
-        }
     }
     else if (this->isPlace())
     {
         std::cout << "  Place : ";
-        if (type_ == TypeHex::PHabitation){
-                texte_couleur(CYAN);
-                std::cout << "Habitation\n";
-            }
-            if (type_ == TypeHex::PMarche){
-                texte_couleur(JAUNE);
-                std::cout << "Marché\n";
-            }
-            if (type_ == TypeHex::PTemple){
-                texte_couleur(MAGENTA);
-                std::cout << "Temple\n";
-            }
-            if (type_ == TypeHex::PCaserne){
-                texte_couleur(ROUGE);
-                std::cout << "Caserne\n";
-            }
-            if (type_ ==  TypeHex::PJardin){
-                texte_couleur(VERT);
-                std::cout << "Jardin\n";
-            }
+    }
+
+    switch (this->getType())
+    {
+    case TypeHex::Carriere:
+        texte_couleur(BLEU);
+        std::cout << "  Carrière\n";
+        break;
+    case TypeHex::Habitation:
+    case TypeHex::PHabitation:
+        texte_couleur(CYAN);
+        std::cout << "Habitation\n";
+        break;
+    case TypeHex::Marche:
+    case TypeHex::PMarche:
+        texte_couleur(JAUNE);
+        std::cout << "Marché\n";
+        break;
+    case TypeHex::Temple:
+    case TypeHex::PTemple:
+        texte_couleur(MAGENTA);
+        std::cout << "Temple\n";
+        break;
+    case TypeHex::Caserne:
+    case TypeHex::PCaserne:
+        texte_couleur(ROUGE);
+        std::cout << "Caserne\n";
+        break;
+    case TypeHex::Jardin:
+    case TypeHex::PJardin:
+        texte_couleur(VERT);
+        std::cout << "Jardin\n";
+        break;
     }
     texte_reset();
 }
