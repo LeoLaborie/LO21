198d1039794fa5c4bcfb50bcad247b839fd6d549
boucanier <chironjules.78@gmail.com>
2025-11-18 17:44:04 +0100

    Doc de Chantier

--- include/Chantier.h
+++ include/Chantier.h
@@ -6,26 +6,60 @@
 #include "Tuile.h"
 #include "couleurs_console.h"
 
+/**
+ * @class Chantier
+ * @brief Représente le chantier contenant les tuiles disponibles pour les joueurs.
+ */
 class Chantier
 {
 private:
     std::vector<Tuile> tuilesChantier;
 
 public:
-
-    //constructeur/destructeur
+    /**
+     * @brief Constructeur par défaut de Chantier
+     */
     Chantier() {};
+
+    /**
+     * @brief Destructeur de Chantier
+     */
     ~Chantier() = default;
 
-    //getters
+    // Getters
+
+    /**
+     * @brief Retourne le nombre de tuiles dans le chantier
+     * @return int : nombre de tuiles dans le chantier
+     */
     int getTaille() const { return tuilesChantier.size(); }
+
+    /**
+     * @brief Retourne les tuiles du chantier
+     * @return const std::vector<Tuile>& : référence constante vers le vecteur de tuiles
+     */
     const std::vector<Tuile> &getTuiles() const { return tuilesChantier; }
 
-    //manipulations
+    // Manipulations
+
+    /**
+     * @brief Ajoute une tuile au chantier
+     * @param t Tuile à ajouter
+     */
     void ajouterTuile(const Tuile &t) { tuilesChantier.push_back(t); }
+
+    /**
+     * @brief Retire une tuile du chantier
+     * @param id Identifiant de la tuile à retirer
+     */
     void retirerTuile(int id);
 
-    //surchage
+    /**
+     * @brief Surcharge de l'opérateur d'affichage pour le Chantier
+     * @param os Flux de sortie
+     * @param c Chantier à afficher
+     * @return Flux de sortie modifié
+     */
     friend std::ostream& operator<<(std::ostream& os,const Chantier& c){
         os << "\nChantier contient " << c.tuilesChantier.size() << " tuiles :\n";
         for (size_t i = 0; i < c.tuilesChantier.size(); i++) {
