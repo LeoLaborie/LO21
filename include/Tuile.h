f5b8d0810f5faa42fd65151b3dab914e2c27ae80
boucanier <chironjules.78@gmail.com>
2025-11-19 21:29:50 +0100

    Doc de Tuile

--- include/Tuile.h
+++ include/Tuile.h
@@ -7,55 +7,116 @@
 #include <algorithm>
 #include "Hexagone.h"
 
-struct OffsetQR {
+struct OffsetQR
+{
     int q;
     int r;
 };
 
-inline void rotation60deg(OffsetQR& o) {
+inline void rotation60deg(OffsetQR &o)
+{
     int q = o.q, r = o.r;
     o.q = -r;
     o.r = q + r;
 }
 
-class Tuile {
+/**
+ * @class Tuile
+ * @brief Représente une tuile composée de plusieurs hexagones
+ */
+class Tuile
+{
 private:
-    std::vector<Hexagone*> hex;
+    std::vector<Hexagone *> hex;
     std::vector<OffsetQR> offsets;
 
 public:
-    //constructeurs et destructeurs
+    /**
+     * @brief Constructeur par défaut de Tuile
+     */
     Tuile() = default;
-    ~Tuile()=default;
-    Tuile(Hexagone* hex1, Hexagone* hex2, Hexagone* hex3);
-    Tuile(Hexagone* hex1, Hexagone* hex2, Hexagone* hex3, Hexagone* hex4);
 
-    //getters
-    const std::vector<Hexagone*>& getHexagones() const { return hex; }
-    const std::vector<OffsetQR>&  getOffsets()   const { return offsets; }
+    /**
+     * @brief Destructeur de Tuile
+     */
+    ~Tuile() = default;
 
-    //manipulations de la tuile
-    void pivoterTuile() {
-        for (auto& o : offsets) rotation60deg(o);
+    /**
+     * @brief Constructeur de Tuile avec trois hexagones
+     * @param hex1 Pointeur vers le premier hexagone
+     * @param hex2 Pointeur vers le deuxième hexagone
+     * @param hex3 Pointeur vers le troisième hexagone
+     */
+    Tuile(Hexagone *hex1, Hexagone *hex2, Hexagone *hex3);
+
+    /**
+     * @brief Constructeur de Tuile avec quatre hexagones
+     * @param hex1 Pointeur vers le premier hexagone
+     * @param hex2 Pointeur vers le deuxième hexagone
+     * @param hex3 Pointeur vers le troisième hexagone
+     * @param hex4 Pointeur vers le quatrième hexagone
+     */
+    Tuile(Hexagone *hex1, Hexagone *hex2, Hexagone *hex3, Hexagone *hex4);
+
+    // Getters
+
+    /**
+     * @brief Retourne les hexagones de la tuile
+     * @return const std::vector<Hexagone *>& : référence constante vers le vecteur des hexagones
+     */
+    const std::vector<Hexagone *> &getHexagones() const { return hex; }
+
+    /**
+     * @brief Retourne les offsets de la tuile
+     * @return const std::vector<OffsetQR>& : référence constante vers le vecteur des offsets
+     */
+    const std::vector<OffsetQR> &getOffsets() const { return offsets; }
+
+    // Manipulations de la tuile
+
+    /**
+     * @brief Pivote la tuile de 60 degrés
+     */
+    void pivoterTuile()
+    {
+        for (auto &o : offsets)
+            rotation60deg(o);
     }
 
-    void pivoterTuile(int k) {
+    /**
+     * @brief Pivote la tuile de k * 60 degrés
+     * @param k Nombre de rotations de 60 degrés à effectuer
+     */
+    void pivoterTuile(int k)
+    {
         k %= 6;
         for (int i = 0; i < k; ++i)
-            for (auto& o : offsets) rotation60deg(o);
+            for (auto &o : offsets)
+                rotation60deg(o);
     }
 
-    //surchage opérateur
-    friend std::ostream& operator<<(std::ostream& os, const Tuile& t) {
-        if (t.hex.empty()) return os << "(tuile vide)\n";
+    /**
+     * @brief Surcharge de l'opérateur de flux pour afficher la tuile
+     * @param os Flux de sortie
+     * @param t Tuile à afficher
+     * @return std::ostream& : référence vers le flux de sortie modifié
+     */
+    friend std::ostream &operator<<(std::ostream &os, const Tuile &t)
+    {
+        if (t.hex.empty())
+            return os << "(tuile vide)\n";
         std::map<int, std::vector<size_t>> lignes;
         for (size_t i = 0; i < t.offsets.size(); ++i)
             lignes[t.offsets[i].r].push_back(i);
-        for (auto& [r, id] : lignes) {
-            std::sort(id.begin(), id.end(),[&](size_t a, size_t b){ return t.offsets[a].q < t.offsets[b].q; });
-            for (size_t k = 0; k < id.size(); ++k) {
+        for (auto &[r, id] : lignes)
+        {
+            std::sort(id.begin(), id.end(), [&](size_t a, size_t b)
+                      { return t.offsets[a].q < t.offsets[b].q; });
+            for (size_t k = 0; k < id.size(); ++k)
+            {
                 t.hex[id[k]]->afficher(true);
-                if (k + 1 < id.size()) os << ' ';
+                if (k + 1 < id.size())
+                    os << ' ';
             }
             os << '\n';
         }
