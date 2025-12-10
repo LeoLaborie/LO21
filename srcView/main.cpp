9571b175b34e56f27de3af7d476cc2e852aa41dd
thibault-de-boutray <thibault.de-boutray@etu.utc.fr>
2025-12-10 23:11:04 +0100

    ajout d'un bloc try et catch pour le mode console et du parametre -C -c pour lancer le jeu en mode console

--- srcView/main.cpp
+++ srcView/main.cpp
@@ -1,11 +1,20 @@
 #include <QApplication>
 #include "mainwindow.h"
-
+#include <string>
+#include "ControleConsole.h"
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
-    MainWindow w;
-    w.resize(1920, 1080);
-    w.show();
-    return app.exec();
+    std::string argument="";
+    if (argc>1){argument = argv[1];}
+    if (argument=="-C" || argument=="-c"){
+        controleConsole();
+        return 0;
+    }
+    else{
+        QApplication app(argc, argv);
+        MainWindow w;
+        w.resize(1920, 1080);
+        w.show();
+        return app.exec();
+    }
 }
